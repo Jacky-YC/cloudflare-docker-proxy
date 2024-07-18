d2d7c6da0c046fda3cca6c76dce3d1521c47587e
jacky <42370375+Jacky-YC@users.noreply.github.com>
2024-07-18 10:52:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,18 +8,22 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
-
-  // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "cloudflare-docker-proxy.chengye713.workers.dev": dockerHub,
 };
+
+// const routes = {
+//   // production
+//   "docker.lixd.xyz": dockerHub,
+//   "quay.lixd.xyz": "https://quay.io",
+//   "gcr.lixd.xyz": "https://gcr.io",
+//   "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
+//   "k8s.lixd.xyz": "https://registry.k8s.io",
+//   "ghcr.lixd.xyz": "https://ghcr.io",
+//   "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+
+//   // staging
+//   "docker-staging.lixd.xyz": dockerHub,
+// };
 
 function routeByHosts(host) {
   if (host in routes) {
